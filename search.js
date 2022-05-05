f11a7a17b28e143ec6f5bd346707cf5ae45b5ac5
Surya Kumara <suryakumara33@gmail.com>
2022-05-05 23:16:54 +0800

    add: naive string search

--- search.js
+++ search.js
@@ -1,3 +1,4 @@
+// O(n)
 const linearSearch = (arr, value) => {
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] === value) {
@@ -8,9 +9,27 @@ const linearSearch = (arr, value) => {
 };
 
 const result = linearSearch([22, 4, 5, 6, 5, 3], 15);
-console.log(result);
+// console.log(result);
 
-const binarySearch = (arr, value) => {
-  let start;
-  let end;
+// binary search must be sort at first
+const binarySearch = (arr, target) => {
+  let start = 0;
+  let end = arr.length - 1;
+  let middle = Math.floor((start + end) / 2);
+
+  while (arr[middle] !== target && start <= end) {
+    if (target < arr[middle]) {
+      end = middle - 1;
+    } else {
+      start = middle + 1;
+    }
+    middle = Math.floor((start + end) / 2);
+  }
+  if (arr[middle] === target) {
+    return middle;
+  }
+  return -1;
 };
+
+const result_binary = binarySearch([2, 5, 6, 9, 13, 15, 28, 30], 50);
+console.log(result_binary);
